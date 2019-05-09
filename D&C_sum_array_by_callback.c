eebc1f22c8a68676d39edc18acba5cc0ebe9924b
wuhy1213 <wuhy1213@163.com>
2019-05-09 16:50:04 +0800

    Update D&C_sum_array_by_callback.c

--- D&C_sum_array_by_callback.c
+++ D&C_sum_array_by_callback.c
@@ -3,7 +3,7 @@ int sum(int *array, int index)
 	int sumValue;
 	if (0 == index){		//基线条件
 		return 0;
-	}else{                  //缩小问题的规模使其符合基线条件
+	}else{					//缩小问题的规模使其符合基线条件
 		sumValue = array[index - 1] + sum(array, index - 1);
 		return sumValue;
 	}
